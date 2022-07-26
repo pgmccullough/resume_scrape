b54642522007f1c0b8a881b6b60c5bee14d747ba
Patrick Glendon McCullough <p@mccullo.ug>
2022-07-26 15:10:33 -0400

    refactor

--- index.js
+++ index.js
@@ -2,28 +2,86 @@ const fs = require('fs');
 const {OCR_API: ocrKey} = require('./apikey.json');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const { ocrSpace } = require('ocr-space-api-wrapper');
-const path = (process.pkg) ? process.cwd()+'/desktop/resumes' : __dirname+'/desktop/resumes';
+const { kill } = require('process');
+//const path = (process.pkg) ? process.cwd()+'/desktop/resumes' : __dirname+'/desktop/resumes';
+const path = (process.pkg) ? process.cwd() : __dirname;
 
 let files = fs.readdirSync(path);
 
 const safeExt = ['pdf','jpg','png','jpeg','bmp','gif','tif','tiff','webp'];
-let promises = [];
 let output = [];
+let myConsole;
+let killSwitch = [];
+
+const titleMsg = () => {
+    console.log("Resume Scraper v1.0");
+}
+
+const sysPrint = (msg, perc) => {
+    perc = Math.floor(perc/10);
+    let x = 0;
+    let stars = ""
+    while (x<10) {
+        if(x<perc) {
+            stars = stars+"*";
+        } else {
+            stars = stars+" "
+        }
+        x++;
+    }
+    stars = "["+stars+"]";
+    clearInterval(myConsole);
+    myConsole = setInterval(
+        () => {
+            console.clear();
+            msg = msg+" . ";
+            titleMsg();
+            console.log("Processing resumes: "+stars);
+            console.log(msg);
+        }
+    ,500)
+}
 
 const phoneFormat = (phone) => {
+    if(phone) {
     phone = phone.replaceAll(" ","");
     phone = phone.replaceAll("-","");
     phone = phone.replaceAll("+1","");
-    phone = phone.slice(0,3)+"-"+phone.slice(3,6)+"-"+phone.slice(7,10)
+    phone = phone.slice(0,3)+"-"+phone.slice(3,6)+"-"+phone.slice(6,10)
     return phone;
+    } else {
+        return "NA";
+    }
 } 
 
-console.log("Reading resumes, please wait. This may take several seconds.");
+const writeCSV = () => {
+    let fileName = path+'/resume_autogen_'+Date.now()+'.csv';
+    clearInterval(myConsole);
+    console.clear();
+    console.log("Generating "+fileName+". [**********]");
+    const csvWriter = createCsvWriter({
+        path: fileName,
+        header: [
+          {id: 'name', title: 'Name'},
+          {id: 'phone', title: 'Phone'},
+          {id: 'email', title: 'Email'}
+        ]
+      });
+        csvWriter
+        .writeRecords(output)
+        .then(() => {
+            clearInterval(myConsole);
+            console.clear();
+            titleMsg();
+            if(killSwitch.kill) console.log("Critical error at resume "+killSwitch.kill+" of "+resumes.length+". Generated CSV will contain details of prior resumes. This error is likely related to overuse of the API, blocking future requests for the next hour.");
+            console.log(fileName+' generated. [**********]');
+        });
+}
 
-files.map(file => {
-    let extCheck = file.split(".").at(-1);
-    if(!safeExt.includes(extCheck)) return;
-    let promise = ocrSpace(path+"/"+file, { apiKey: ocrKey })
+const processResume = (i) => {
+    sysPrint("Reading "+resumes[i]+". "+(i+1)+"/"+resumes.length,(i+1)/(resumes.length)*100);
+
+    ocrSpace(path+"/"+resumes[i], { apiKey: ocrKey })
     .then(response => {
         let lines = response.ParsedResults[0].ParsedText.split(/\r?\n|\r|\n/g);
         let name = lines[0];
@@ -43,23 +101,22 @@ files.map(file => {
         }
         phone = phoneFormat(phone);
         output.push({email,name,phone});
-    });
-    promises.push(promise);
-})
+    }).catch(_response => {
+        killSwitch = {kill: i}
+    }).finally(_response => {
+        if(killSwitch.kill) return writeCSV();
+        if(i<resumes.length-1) {
+            processResume(i+1);
+        } else {
+            writeCSV();
+        }
+    })
+}
 
-Promise.all(promises)
-.then(_res => {
-    let fileName = path+'/resume_autogen_'+Date.now()+'.csv';
-    const csvWriter = createCsvWriter({
-        path: fileName,
-        header: [
-          {id: 'name', title: 'Name'},
-          {id: 'phone', title: 'Phone'},
-          {id: 'email', title: 'Email'}
-        ]
-      });
-      
-    csvWriter
-    .writeRecords(output)
-    .then(()=> console.log(fileName+' written successfully.'));
-})
+
+
+
+
+const resumes = files.filter(file => safeExt.includes(file.split(".").at(-1)));
+sysPrint("Processing "+resumes.length+" resumes.",0);
+processResume(0);
