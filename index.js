565a3727701ecf1ee703ee16793a62c01a1b11cc
Patrick Glendon McCullough <p@mccullo.ug>
2022-07-26 17:20:44 -0400

    Remove manual escape handling

--- index.js
+++ index.js
@@ -154,26 +154,3 @@ if(fs.existsSync("timeout.json")) {
 }
 if(startI>resumes.length) startI = 0;
 processResume(startI||0);
-
-
-
-
-process.stdin.resume();//so the program will not close instantly
-
-function exitHandler(options, exitCode) {
-    killSwitch = {kill: storeI}
-    writeCSV();
-}
-
-//do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
-
-//catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-
-// catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
-
-//catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
